b0a06b6de795610db6034f02bfb6b4c4178bb9c8
Lukmon492 <lukmon492@gmail.com>
2023-01-14 22:58:37 +0100

    initial commit

---
+++ portfolio-template/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", background: "#646cff", color: "#fff" }}>
+        Portfolio Website
+      </h1>
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio-template/src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./hero.css";
+const Hero = () => {
+  return (
+    <div className="container">
+      <div className="right-hero">
+        {/* <div className="hero">
+      <div className="hero-body">
+      <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+          <h1 className="hero-title"></h1> */}
+      </div>
+      <div className="middle-hero">
+        <div className="user-info">
+          <h1 className="user-name">
+            I’m Rayan Adlrdard <span className="color-text">Font - End</span>
+            Developer
+          </h1>
+        </div>
+      </div>
+      <div className="left-hero"></div>
+    </div>
+  );
+};
+
+export default Hero;
