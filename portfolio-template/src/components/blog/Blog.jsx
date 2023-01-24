704b82d1f7158912f280ef4783989df1bd82ace6
Lukmon492 <lukmon492@gmail.com>
2023-01-24 22:53:24 +0100

    iteration added

--- portfolio-template/src/components/blog/Blog.jsx
+++ portfolio-template/src/components/blog/Blog.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./blog.css";
-import landingP from "../../assets/landingP.png";
+// import landingP from "../../assets/landingP.png";
 
 const Blog = () => {
+  const blogs = [
+    {
+      img: "../../assets/landingP.png",
+      description: "landing page",
+      title: "How to make web tempates",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna",
+      explore: "Learn more",
+    },
+    {
+      img: "../../assets/card.jpeg",
+      description: "bussiness card",
+      title: "How to make web tempates",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna",
+      explore: "Learn more",
+    },
+    {
+      img: "../../assets/flyer.jpeg",
+      description: "flyer",
+      title: "How to make web tempates",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna",
+      explore: "Learn more",
+    },
+  ];
   return (
     <div className="blog__section">
       <h2 className="blog__header">Blog</h2>
@@ -11,33 +34,40 @@ const Blog = () => {
         sint. Velit officia consequat duis enim velit mollit. lorem ipsum
       </p>
       <div className="blog__section-blocks">
-        <div className="blog__section-block">
+        {blogs.map((blog, item) => (
+          <div className="blog__section-block" key={item.id}>
+            <img src={blog.img} alt={blog.description} />
+            <div className="blog__section-block__contents">
+              <h4>{blog.title}</h4>
+              <p>{blog.text}</p>
+              <h5>{blog.explore}</h5>
+            </div>
+          </div>
+        ))}
+
+        {/* <div className="blog__section-block">
           <img src={landingP} />
-          <h4>How to make web tempates</h4>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla
-            diam in ac dictum a urna
-          </p>
-          <h4>Learn more</h4>
+
+          <div className="blog__section-block__contents">
+            <h4>How to make web tempates</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae
+              nulla diam in ac dictum a urna
+            </p>
+            <h5>Learn more</h5>
+          </div>
         </div>
         <div className="blog__section-block">
           <img src={landingP} />
-          <h4>How to make web tempates</h4>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla
-            diam in ac dictum a urna
-          </p>
-          <h4>Learn more</h4>
-        </div>
-        <div className="blog__section-block">
-          <img src={landingP} />
-          <h4>How to make web tempates</h4>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla
-            diam in ac dictum a urna
-          </p>
-          <h4>Learn more</h4>
-        </div>
+          <div className="blog__section-block__contents">
+            <h4>How to make web tempates</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae
+              nulla diam in ac dictum a urna
+            </p>
+            <h5>Learn more</h5>
+          </div>
+        </div> */}
       </div>
     </div>
   );
